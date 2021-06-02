7df7567ea8092a9a5791ccb6fa9828c21385ebc3
Alcir Junior <junior@cajucomunica.com.br>
2021-06-02 12:22:08 -0300

    feat: Add Championships Component

--- src/components/Championships/Index.tsx
+++ src/components/Championships/Index.tsx
@@ -1,8 +1,58 @@
-import { Box } from "@chakra-ui/react";
+import { Badge, Box, Image, SimpleGrid, Text, useColorModeValue } from "@chakra-ui/react";
 
-const Championships = () => {
+type Championship = {
+  campeonato_id: number;
+  logo: string;
+  nome: string;
+  nome_popular: string;
+  rodada_atual: {
+    rodada: number;
+    nome: string;
+  },
+  slug: string;
+  status: string;
+};
+
+interface ChampionshipsProps {
+  championships: Championship[];
+};
+
+const Championships = ({ championships }: ChampionshipsProps) => {
+  const bgColor = useColorModeValue('gray.50', 'gray.900');
+  const color = useColorModeValue('gray.900', 'gray.200');
+
   return (
-    <Box>Campeonatos</Box>
+    <SimpleGrid minChildWidth="auto" spacing={20}>
+      {championships.map((championship => (
+        <Box
+          maxW="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          bg={bgColor}
+          color={color}
+          key={championship.campeonato_id}
+        >
+          <Image src={championship.logo} alt={championship.nome_popular} />
+          <Box p={6}>
+            <Box d="flex" alignItems="baseline">
+              <Badge variant="outline" borderRadius="full" px={2} colorScheme="teal">
+                {championship.rodada_atual.nome}
+              </Badge>
+            </Box>
+            <Text
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              mt={2}
+            >
+              {championship.nome}
+            </Text>
+          </Box>
+        </Box>
+      )))}
+    </SimpleGrid>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,33 @@
 import { Container } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 import Championships from "../components/Championships/Index";
 import Header from "../components/Header/Index";
+import api from "../services/api";
 
-const Home = () => {
+const Home = ({ championships }) => {
   return (
     <>
       <Header />
       <Container maxW="container.xl">
-        <Championships />
+        <Championships championships={championships} />
       </Container>
     </>
   )
 }
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  let championships = [];
+
+  await api.get('/campeonatos')
+    .then((response => {
+      championships.push(...response.data);
+    }));
+
+  return {
+    props: {
+      championships
+    }
+  };
+};
